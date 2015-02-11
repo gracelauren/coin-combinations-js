f3642686df6f95049198700c868f3cac47136f51
Grace Mekarski & Ron Park <student@epicodus.com>
2015-02-11 10:25:59 -0800

    add files all specs pass

---
+++ js/scripts.js
@@ -0,0 +1,79 @@
+var coinCombination = function(coins) {
+  var coins = coins;
+  var kennedyHalfDollar = 0;
+  var quarter = 0;
+  var dime = 0;
+  var nickle = 0;
+  var penny = 0;
+  var change = "";
+
+  do {
+    kennedyHalfDollar = Math.floor(coins / 0.50);
+    coins = Math.abs(coins % 0.50);
+  } while (coins >= .50);
+
+  do {
+    quarter = Math.floor(coins / 0.25);
+    coins = Math.abs(coins % 0.25);
+  } while (coins >= .25);
+
+  do {
+    dime = Math.floor(coins / 0.10);
+    coins = Math.abs(coins % 0.10);
+  } while (coins >= .10);
+
+  do {
+    nickle = Math.floor(coins / 0.05);
+    coins = Math.abs(coins % 0.05);
+  } while (coins >= .05);
+
+  do {
+    penny = Math.floor(coins / 0.01);
+    coins = Math.abs(coins % 0.01);
+  } while (coins >= .01);
+
+
+  if ( kennedyHalfDollar > 1 ) {
+    change = change + kennedyHalfDollar + " Kennedy Half Dollars, ";
+  } else if ( kennedyHalfDollar === 1 ) {
+    change = change + kennedyHalfDollar + " Kennedy Half Dollar, ";
+  } else {
+    change = change;
+  }
+
+  if ( quarter > 1 ) {
+    change = change + quarter + " Quarters, ";
+  } else if ( quarter === 1 ) {
+    change = change + quarter + " Quarter, ";
+  } else {
+    change = change;
+  }
+
+  if ( dime > 1 ) {
+    change = change + dime + " Dimes, ";
+  } else if ( dime === 1 ) {
+    change = change + dime + " Dime, ";
+  } else {
+    change = change;
+  }
+
+  if ( nickle > 1 ) {
+    change = change + nickle + " Nickles, ";
+  } else if ( nickle === 1 ) {
+    change = change + nickle + " Nickle, ";
+  } else {
+    change = change;
+  }
+
+  if ( penny > 1 ) {
+    change = change + penny + " Pennies, ";
+  } else if ( penny === 1 ) {
+    change = change + penny + " Penny, ";
+  } else {
+    change = change;
+  }
+
+  change = change.slice(0, change.length -2)
+  return change;
+
+};
---
+++ spec/specs.js
@@ -0,0 +1,31 @@
+describe("coinCombination", function() {
+
+  it('will return to you with one kennedy half dollar if you put in $0.50', function() {
+    expect(coinCombination(0.50)).to.equal("1 Kennedy Half Dollar");
+  });
+
+  it('will return to you with one quarter if you put in $0.25', function() {
+    expect(coinCombination(0.25)).to.equal("1 Quarter");
+  });
+
+  it('will return to you with one dime if you put in $0.10', function() {
+    expect(coinCombination(0.10)).to.equal("1 Dime");
+  });
+
+  it('will return to you with one nickle if you put in $0.05', function() {
+    expect(coinCombination(0.05)).to.equal("1 Nickle");
+  });
+
+  it('will return to you with one penny if you put in $0.01', function() {
+    expect(coinCombination(0.01)).to.equal("1 Penny");
+  });
+
+  it('will return to you with one kennedy half dollar and one quarter if you put in $0.75', function() {
+    expect(coinCombination(0.75)).to.equal("1 Kennedy Half Dollar, 1 Quarter");
+  })
+
+  it('will return to you with one kennedy half dollar and one quarter and 4 pennies if you put in $0.79', function() {
+    expect(coinCombination(0.79)).to.equal("1 Kennedy Half Dollar, 1 Quarter, 4 Pennies");
+  })
+
+});
